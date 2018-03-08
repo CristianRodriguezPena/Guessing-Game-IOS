8c7eebf118bcd6a43cf32c68661566feee50c702
Cristian Rodriguez <cristianrodriguez@Interapts-MacBook-Pro-141.local>
2018-03-08 16:00:43 -0500

    add user input functionality

--- GuessingGameIOS/ViewController.swift
+++ GuessingGameIOS/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var UserGuessField: UITextField!
     
     var randomNumber = Int(arc4random_uniform(100))
-    
+    var userGuess :Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,7 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func guessButtonPress(_ sender: Any) {
-        
+        userGuess = Int(UserGuessField.text!)
+        print(userGuess)
     }
     
 
