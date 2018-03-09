2f1bfc35a22476626fd80657638fc23f9c5632ec
Cristian Rodriguez <cristianrodriguez@Interapts-MacBook-Pro-141.local>
2018-03-08 21:14:34 -0500

    added functionality

--- GuessingGameIOS/ViewController.swift
+++ GuessingGameIOS/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var UserGuessField: UITextField!
+    @IBOutlet weak var guessesRemaining: UILabel!
+    @IBOutlet weak var checking: UILabel!
+    @IBOutlet weak var responseToUser: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
     
     var randomNumber = Int(arc4random_uniform(100))
     var userGuess :Int!
+    var guessesLeft = 0 {
+        didSet {
+            guessesRemaining.text = "\(guessesLeft)"
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        guessesLeft = 5
+        checking.text = ""
+        checking.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 0.61)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,9 +38,34 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func guessButtonPress(_ sender: Any) {
-        userGuess = Int(UserGuessField.text!)
+
+    @IBAction func guessButton(_ sender: Any) {
+        if guessesLeft == 0 {
+            randomNumber = Int(arc4random_uniform(100))
+            guessesLeft = 5
+            checkButton.setTitle("Guess!", for: UIControlState(rawValue: 0))
+            responseToUser.text = ""
+            return
+        }
+        userGuess = Int(UserGuessField.text!) ?? -1
+        UserGuessField.text = ""
         print(userGuess)
+        guessesLeft -= 1
+        if userGuess == randomNumber {
+            responseToUser.text = "You got it, good job!"
+        } else {
+            if guessesLeft == 0 {
+                responseToUser.text = "You have lost, the number was \(randomNumber), press the 'Restart' button to begin another game"
+                checkButton.setTitle("Restart", for: UIControlState(rawValue: 0))
+            } else {
+                checking.text = "WRONG!!"
+                responseToUser.text = "That number was too " + (userGuess < randomNumber ? "Low" : "High")
+                checking.text = ""
+            }
+        }
+    }
+    
+    @IBAction func Testing(_ sender: Any) {
     }
     
 
